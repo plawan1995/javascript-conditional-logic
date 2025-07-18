8407dcc648159e56ee801662748414784696f00b
plawan1995 <jnyjame12@gmail.com>
2025-07-18 14:00:49 +0700

    fix: update light bulb status checks for accurate condition handling

--- ex-2.js
+++ ex-2.js
@@ -3,7 +3,13 @@ let lightBulbStatus = "On";
 lightBulbStatus = "Broken";
 
 // Start coding here.
-if (lightBulbStatus === "Broken"){
+if (lightBulbStatus == "on"){
+    console.log("Light bulb is On.");
+}
+else if (lightBulbStatus === "Off"){
+    console.log("Light bulb is Off.");
+}
+else if (lightBulbStatus === "Broken"){
     console.log("Light bulb is Broken.");
     
 }
