160281503f70c2363a33a5ace75dc72e1b9146ca
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-07-18 13:58:10 +0700

    feat: finished condition-logic assignment

--- ex-4.js
+++ ex-4.js
@@ -1,5 +1,16 @@
-//Exercise 4
-// Switch Statement
-let lightBulbStatus = "On";
+// Exercise #4: Switch the Light Bulb
 
-//Start coding here
+let lightBulbStatus = "Broken";
+switch(lightBulbStatus){
+    case "On":
+    console.log("Light bulb is On.");
+    break;
+    case "Off":
+    console.log("Light bulb is Off.");
+    break;
+    case "Broken":
+    console.log("Light bulb is Broken.");
+    break;
+    default:
+    console.log(`Please choose the correct input (On/Off/Broken)`);       
+}
--- ex-5.js
+++ ex-5.js
@@ -1,16 +1,27 @@
-function calculateStudentGrade(score) {
-  // Start coding here
-}
+// Exercise #5: Student Grade Calculation
 
-// Example case
-let johnGrade = calculateStudentGrade(30);
-let lisaGrade = calculateStudentGrade(73);
-let janeGrade = calculateStudentGrade(80);
-let jjGrade = calculateStudentGrade(60);
-let alexGrade = calculateStudentGrade(90);
+      function calculateStudentGrade(score) {
+          if(score>=90 && score<=100){
+              return "A";
+          }else if(score>=80 && score<=89){
+              return "B";
+          }else if(score>=70 && score<=79){
+              return "C";
+          }else if(score>=60 && score<=69){
+              return "D";
+          }else{
+              return "F";
+          }
+        }
 
-console.log(`John grade is ${johnGrade}`);
-console.log(`Lisa grade is ${lisaGrade}`);
-console.log(`Jane grade is ${janeGrade}`);
-console.log(`JJ grade is ${jjGrade}`);
-console.log(`Alex grade is ${alexGrade}`);
+        let johnGrade = calculateStudentGrade(30);
+        let lisaGrade = calculateStudentGrade(73);
+        let janeGrade = calculateStudentGrade(80);
+        let jjGrade = calculateStudentGrade(60);
+        let alexGrade = calculateStudentGrade(90);
+        
+      console.log(`John grade is ${johnGrade}`);
+      console.log(`Lisa grade is ${lisaGrade}`);
+      console.log(`Jane grade is ${janeGrade}`);
+      console.log(`JJ grade is ${jjGrade}`);
+      console.log(`Alex grade is ${alexGrade}`);
